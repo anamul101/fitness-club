341605e9c6cf389cd13eca60fef4b3116c65a1c7
anamul101 <rjanamul01871@gmail.com>
2022-09-29 16:36:44 +0100

    favicone

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div>
             <div className="navbar bg-gray-900 text-white sticky top-0">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-4xl font-semibold">Fn Yoga <span className='text-rose-600 mr-2'>Club</span></a>
+                    <a href='/' className="btn btn-ghost normal-case text-4xl font-semibold">Fn Yoga <span className='text-rose-600 mr-2'>Club</span></a>
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal p-0 text-lg font-semibold">
